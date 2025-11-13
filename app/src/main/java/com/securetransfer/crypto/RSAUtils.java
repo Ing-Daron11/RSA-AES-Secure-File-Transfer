8c10ab5cee968b08afdf15764cf518f87c364a00
Ing.Daron11 <daronmercadog@gmail.com>
2025-11-13 07:49:55 -0500

    feat: add generation of pairs of keys RSA

--- app/src/main/java/com/securetransfer/crypto/RSAUtils.java
+++ app/src/main/java/com/securetransfer/crypto/RSAUtils.java
@@ -1,6 +1,68 @@
 package com.securetransfer.crypto;
 
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.NoSuchAlgorithmException;
+import javax.crypto.Cipher;
+import java.util.Base64;
+
 public class RSAUtils {
-    // Métodos: generarParClaves(), cifrarConPublica(), descifrarConPrivada()
-    
+
+    private static final String ALGORITHM = "RSA";
+    private static final int KEY_SIZE = 2048;
+
+    private PublicKey publicKey;
+    private PrivateKey privateKey;
+
+    /**
+     * Generate a new pair of keys (public and private)
+     */
+    public void generateKeyPair() throws NoSuchAlgorithmException {
+        KeyPairGenerator keyGen = KeyPairGenerator.getInstance(ALGORITHM);
+        keyGen.initialize(KEY_SIZE);
+        KeyPair keyPair = keyGen.generateKeyPair();
+
+        this.publicKey = keyPair.getPublic();
+        this.privateKey = keyPair.getPrivate();
+    }
+
+    /**
+     * Returns the public key in Base64 format (to send to the client)
+     */
+    public String getPublicKeyAsString() {
+        return Base64.getEncoder().encodeToString(publicKey.getEncoded());
+    }
+
+    /**
+     * Encrypts a text using the public key
+     */
+    public String encryptWithPublic(String plainText) throws Exception {
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+        byte[] encryptedBytes = cipher.doFinal(plainText.getBytes());
+        return Base64.getEncoder().encodeToString(encryptedBytes);
+    }
+
+    /**
+     * Decrypts a text using the private key
+     */
+    public String decryptWithPrivate(String encryptedText) throws Exception {
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.DECRYPT_MODE, privateKey);
+        byte[] decodedBytes = Base64.getDecoder().decode(encryptedText);
+        byte[] decryptedBytes = cipher.doFinal(decodedBytes);
+        return new String(decryptedBytes);
+    }
+
+    // ======= Getters =========
+
+    public PublicKey getPublicKey() {
+        return publicKey;
+    }
+
+    public PrivateKey getPrivateKey() {
+        return privateKey;
+    }
 }
