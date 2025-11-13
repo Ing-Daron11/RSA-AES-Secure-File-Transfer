76f53f2f5e367bcd73d89d33d7ca204b4bde74c4
Ing.Daron11 <daronmercadog@gmail.com>
2025-11-13 07:54:50 -0500

    test: add a little test to see behaviour

--- app/src/main/java/com/securetransfer/server/Server.java
+++ app/src/main/java/com/securetransfer/server/Server.java
@@ -1,13 +1,31 @@
 package com.securetransfer.server;
 
+import com.securetransfer.crypto.RSAUtils;
+
 public class Server {
 
     public static void main(String[] args){
         System.out.println("|===================|");
         System.out.println("| Iniciando Servidor|");
         System.out.println("|===================|");
-        //Aquí tiene que ir la logica de la generación RSA, recepción de datos y verificación de integridad.
+        
+        try {
+            RSAUtils rsa = new RSAUtils();
+            rsa.generateKeyPair();
 
+            String mensajeOriginal = "Clave AES temporal";
+            System.out.println("Texto original: " + mensajeOriginal);
+
+            String cifrado = rsa.encryptWithPublic(mensajeOriginal);
+            System.out.println("Texto cifrado (Base64): " + cifrado);
+
+            String descifrado = rsa.decryptWithPrivate(cifrado);
+            System.out.println("Texto descifrado: " + descifrado);
+
+            System.out.println("Prueba RSA completada correctamente.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
     
 }
